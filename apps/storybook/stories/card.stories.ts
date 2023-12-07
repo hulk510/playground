b6517ec250b82824c5570e46eda0b7618100d8d8
hulk510 <dorian.51069@gmail.com>
2023-12-07 12:49:37 +0900

    packageインポートしてみてchromatic入れてみた

---
+++ apps/storybook/stories/card.stories.ts
@@ -0,0 +1,24 @@
+import { Card } from '@repo/ui/card';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Example/Card',
+  component: Card,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+} as Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: 'hello world',
+    children: 'open google',
+    href: 'https://google.com',
+  },
+};
