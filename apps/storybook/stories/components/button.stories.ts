cbd01c106b304021e798a361c39ecd5df2c2d133
hulk510 <dorian.51069@gmail.com>
2023-12-16 01:16:54 +0900

    chore: 適当にコンポーネント追加

---
+++ apps/storybook/stories/components/button.stories.ts
@@ -0,0 +1,19 @@
+import { Client } from '@repo/ui';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  component: Client,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Client>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
