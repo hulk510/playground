99cd06fe61b0cd9fe51d32ba6a4b52b7ecbcb2be
hulk510 <dorian.51069@gmail.com>
2024-01-14 16:58:23 +0900

    fix: eslint

--- apps/blogs/.eslintrc.js
+++ apps/blogs/.eslintrc.js
@@ -1,12 +1,12 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
+  extends: ["@repo/eslint-config/react-internal.js"],
   env: {
     node: true,
     es2022: true,
     browser: true,
   },
-  extends: ["eslint:recommended", "plugin:astro/recommended"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
