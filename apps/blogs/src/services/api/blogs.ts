43544a04219e22e83fd15072932b35494815be5d
hulk510 <dorian.51069@gmail.com>
2024-01-14 16:41:17 +0900

    deps: dotenv入れた

--- apps/blogs/src/services/api/blogs.ts
+++ apps/blogs/src/services/api/blogs.ts
@@ -3,7 +3,7 @@ import type { Blog } from "./types";
 
 export async function getBlogs(query: any = null) {
   return await getPayloadCollection<Blog>(
-    `${process.env.PAYLOAD_CMS_URL}/api/blogs`,
+    `${import.meta.env.PAYLOAD_CMS_URL}/api/blogs`,
     query
   );
 }
