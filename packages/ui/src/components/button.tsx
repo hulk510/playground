b33abf3ddfd329690ab86d4beecad66dfab28973
hulk510 <dorian.51069@gmail.com>
2024-05-21 13:13:53 +0900

    fix: ミスってたので修正

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -6,15 +6,15 @@ const green = css`
   background: green;
 `;
 
-const blue = css`
-  background: blue;
+const red = css`
+  background: red;
 `;
 
 export function Client() {
   const [checked, toggle] = useReducer((state) => !state, false);
 
   return (
-    <Box className={checked ? green : blue} p={12}>
+    <Box className={checked ? green : red} p={12}>
       <Button onClick={() => toggle()}>Change Background Color</Button>
     </Box>
   );
