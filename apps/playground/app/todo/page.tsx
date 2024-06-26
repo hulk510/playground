90bb95468b73b84e8269ddd904f96aeb6d927a7b
Haruka <33943897+hulk510@users.noreply.github.com>
2024-06-26 12:22:34 +0900

    feat: todoのcrud作成 (#615)
    * feat: create Todo crud
    
    * chore: 修正

--- apps/playground/app/todo/page.tsx
+++ apps/playground/app/todo/page.tsx
@@ -1,17 +1,17 @@
 import { PrismaClient } from '@repo/db';
-import { ModeToggle } from '@repo/ui/molecules/mode-toggle';
-import { Input } from '@repo/ui/shadcn/input';
+import { TodoForm } from '../ui/todo-form';
+import { TodoItem } from '../ui/todo-item';
 
 export default async function Page() {
   const prisma = new PrismaClient();
   const todos = await prisma.todo.findMany();
 
   return (
-    <div className='container mx-auto px-4'>
-      <h1>Hello World</h1>
-      <ModeToggle />
-      <Input placeholder='Something Add Your Todo...' />
-      <pre>{JSON.stringify(todos, null, 2)}</pre>
+    <div className='px-4 container mx-auto space-y-8'>
+      <TodoForm />
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
     </div>
   );
 }
