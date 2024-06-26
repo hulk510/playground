2dcb9a753cb825db28ac3234ee8fcaf512d5f33e
hulk510 <dorian.51069@gmail.com>
2024-06-26 16:06:47 +0900

    fix: blogのテンプレートがおかしかったので修正 tagの選択肢追加

--- apps/web/turbo/generators/config.ts
+++ apps/web/turbo/generators/config.ts
@@ -13,17 +13,23 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
         {
           type: "input",
           name: "slug",
-          message: "記事のslugは何にしますか?",
+          message: "記事slugを入力してください",
         },
         {
           type: "input",
           name: "title",
-          message: "記事のタイトルは何にしますか",
+          message: "記事タイトルを入力してください",
         },
         {
           type: "input",
           name: "description",
-          message: "記事の説明は何にしますか",
+          message: "記事を表す概要説明は何にします？",
+        },
+        {
+          name: "tags",
+          type: "checkbox",
+          message: "記事にタグつけますかー？",
+          choices: ["playground", "適当"], // TODO: タグを増やす どういうタグがいいのか？
         },
       ],
       actions: [
