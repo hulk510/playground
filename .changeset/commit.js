5ccc3f0dde11dd4367d1089143e3e9f15938af18
hulk510 <dorian.51069@gmail.com>
2024-08-19 09:25:57 +0900

    chore: configのcommitいらないので削除

--- .changeset/commit.js
+++
@@ -1,9 +0,0 @@
-// commit.cjs
-const commit = require('@changesets/cli/commit');
-
-module.exports = {
-  getAddMessage(changeset, options) {
-    return `docs(changeset): ${changeset.summary}`;
-  },
-  getVersionMessage: commit.default.getVersionMessage,
-};
