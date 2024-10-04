936e22ea8a2251ddb3bb53414e2c60870721dfe1
Haruka <33943897+hulk510@users.noreply.github.com>
2024-10-04 13:39:00 +0900

    fix: 使ってないvariables削除 (#1074)

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,13 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "ap-northeast-1"
 }
-
-variable "docker_username" {
-  description = "Dockerhub username"
-  type        = string
-}
-
-variable "docker_password" {
-  description = "Dockerhub password"
-  type        = string
-}
